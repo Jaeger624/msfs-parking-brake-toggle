64f5824fef0d3a240a9f20bf9a431ec85df6c0c6
Paul Wenzlaff <Paulwenzlaff@gmail.com>
2021-06-26 13:25:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # msfs-parking-brake-toggle
+
+- You need to install the latest version of Visual Studio including the "Desktop development with C++ workload"
---
+++ src/main.js
@@ -0,0 +1,21 @@
+const fsuipc = require('fsuipc');
+const obj = new fsuipc.FSUIPC();
+
+obj.open()
+    .then((obj) => {
+      console.log(obj.add('clockHour', 0x238, fsuipc.Type.Byte));
+      console.log(obj.add('aircraftType', 0x3D00, fsuipc.Type.String, 256));
+      console.log(obj.add('lights', 0x0D0C, fsuipc.Type.BitArray, 2));
+
+      return obj.process();
+    })
+    .then((result) => {
+      console.log(JSON.stringify(result));
+
+      return obj.close();
+    })
+    .catch((err) => {
+      console.error(err);
+
+      return obj.close();
+    });
